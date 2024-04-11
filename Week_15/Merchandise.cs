67a6b099bf3d733233fe0dc6576f9cb0c291fa2e
MHLa <mhla63314@edu.ucl.dk>
2024-04-11 09:55:25 +0200

    Second tasklist initialization

---
+++ Week_15/Merchandise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Merchandise
+    {
+        private string _itemId;
+
+        public string ItemId
+        {
+            get { return _itemId; }
+            set { _itemId = value; }
+        }
+
+        public Merchandise(string itemId) 
+        {
+            _itemId = itemId;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
